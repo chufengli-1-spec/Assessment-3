2b5a7a44157341b8730a498b01aad9147aed91f2
chufengli-1-spec <chufeng.li-1@student.uts.edu.au>
2025-10-17 19:58:51 +1100

    70%

--- Assets/_Scripts/PacStudentController.cs
+++ Assets/_Scripts/PacStudentController.cs
@@ -565,13 +565,14 @@ public class PacStudentController : MonoBehaviour
             case 0:
             case 5:
             case 6:
-            case 8:
+            //case 8:
                 return true;
             case 1:
             case 2:
             case 3:
             case 4:
             case 7:
+            case 8:
             default:
                 return false;
         }
